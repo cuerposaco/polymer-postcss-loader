cfa57e1faf4f86eb64a3ec2bffb42cf46361b971
Ivan <Mejias@MacBook-Pro-de-Barrabes.local>
2017-11-15 00:38:00 +0100

    craete loader

---
+++ index.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio');
+const postcss = require('postcss');
+const postcssSyntax = require('postcss-html');
+const autoprefixer = require('autoprefixer');
+
+module.exports = function polymerPostcssLoader(source) {
+
+  const loaderCallback = this.async();
+
+  const $domSource = cheerio.load(source);
+
+  const domModuleElement = $domSource('dom-module');
+  const $dom2 = cheerio.load(domModuleElement.html());
+
+  const $domTemplate = cheerio.load($dom2('template').html());
+  const styleTag = $domTemplate('style');
+
+  const styleParser = ($element, callback) => {
+    return postcss([
+      autoprefixer()
+    ]).process(
+      $element.html(),
+      { syntax: postcssSyntax }
+    ).then(function (result) {
+      console.log('cssParser --->', result.content);
+      loaderCallback(null, result.content);
+    });
+  };
+  
+  styleParser($domTemplate).catch(err => {
+    console.log(error);
+    loaderCallback(null, source);
+  });
+}
