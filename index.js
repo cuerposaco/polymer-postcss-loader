58e36998b234eaa866464c9f2e30df8f91047920
Ivan <Mejias@MacBook-Pro-de-Barrabes.local>
2017-11-15 10:45:17 +0100

    minor fixes

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const autoprefixer = require('autoprefixer');
 module.exports = function polymerPostcssLoader(source) {
   const loaderCallback = this.async();
   const htmlFilePath = this.resourcePath;
-  
+
   const getDomModule = parsed => parsed.childNodes
     .find(child => child.nodeName === 'html').childNodes
     .find(child => child.nodeName === 'body').childNodes
     .find(child => child.nodeName === 'dom-module');
 
-  const getStyleTag = _domModule => {
+  const getStyleTag = (_domModule) => {
     return _domModule.childNodes.reduce((acc, item) => {
       // console.log(`_domModule ${item.nodeName} - attrs: ${item.attrs}`)
       if (item.nodeName !== 'style') { return acc; }
       if (item.attrs && item.attrs.length > 0) { return acc; }
       // console.log(`item ${item.nodeName} - nodes: ${item.childNodes.length}`)
-      acc = acc.concat(item.childNodes[0].value);
-      return acc;
+      return [
+        ...acc,
+        item.childNodes[0].value,
+      ];
     }, []);
   };
 
@@ -49,11 +51,11 @@ module.exports = function polymerPostcssLoader(source) {
   if (!template) { return loaderCallback(null, source); }
 
   const styles = getStyleTag(template.content)[0];
-  if (!styles.length) { return loaderCallback(null, source); }
+  if (!styles || !styles.length) { return loaderCallback(null, source); }
   styleParser(styles)
     .then(_styles => loaderCallback(null, fixTemplate(_styles, source)))
-    .catch(err => {
+    .catch((err) => {
       console.log(`[polymerPostcssLoader][${htmlFilePath}] Error: ${err}`);
       loaderCallback(null, source);
     });
-}
+};
