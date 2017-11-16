2561d6cf92335f77a838835cb8e8f646ddc79a8a
Ivan <Mejias@MacBook-Pro-de-Barrabes.local>
2017-11-16 18:46:26 +0100

    many improvements

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const parse5 = require('parse5');
 const postcss = require('postcss');
 const postcssSyntax = require('postcss-html');
 
-
 module.exports = function polymerPostcssLoader(source) {
+  
+  // Loader configuration
+  this.cacheable = false;
   const loaderCallback = this.async();
   const htmlFilePath = this.resourcePath;
   const options = getOptions(this);
+  const postcssPlugins = (options && options.plugins) || [];
 
+  // Loader Helpers
   const getDomModule = parsed => parsed.childNodes
     .find(child => child.nodeName === 'html').childNodes
     .find(child => child.nodeName === 'body').childNodes
@@ -16,35 +20,38 @@ module.exports = function polymerPostcssLoader(source) {
 
   const getStyleTag = (_domModule) => {
     return _domModule.childNodes.reduce((acc, item) => {
-      // console.log(`_domModule ${item.nodeName} - attrs: ${item.attrs}`)
       if (item.nodeName !== 'style') { return acc; }
       if (item.attrs && item.attrs.length > 0) { return acc; }
-      // console.log(`item ${item.nodeName} - nodes: ${item.childNodes.length}`)
       return [
         ...acc,
         item.childNodes[0].value,
       ];
     }, []);
   };
+  
+  const styleParser = styleValue => (
+    postcss(postcssPlugins)
+      .process(
+        styleValue,
+        { syntax: options.syntax || postcssSyntax }
+      )
+      .then(result => result.content)
+      .catch(err => {
+        this.emitWarning(err);
+        loaderCallback(null, source);
+      })
+  );
 
-  const fixTemplate = (postcssRes, _source) => {
-    const template = `<style>${postcssRes}</style>`;
-    return _source
-      .replace(/<style>[\s\S]+<\/style>/g, template);
+  const fixTemplate = (result, mainSource) => {
+    const newStyles = `<style>${result}</style>`;
+    return mainSource
+      .replace(/<style>[\s\S]+<\/style>/g, newStyles);
   };
-  const postcssPlugins = (options && options.plugins) || [];
-  const styleParser = styleValue => postcss(postcssPlugins).process(
-    styleValue,
-    { syntax: postcssSyntax }
-  ).then(result => result.content);
 
-
+  // Loader Processor
   const parsed = parse5.parse(source);
-
   const domModule = getDomModule(parsed);
-  if (!domModule) {
-    return loaderCallback(null, source);
-  }
+  if (!domModule) { return loaderCallback(null, source); }
 
   const template = domModule.childNodes
     .find(child => child.nodeName === 'template');
@@ -52,10 +59,13 @@ module.exports = function polymerPostcssLoader(source) {
 
   const styles = getStyleTag(template.content)[0];
   if (!styles || !styles.length) { return loaderCallback(null, source); }
+  
   styleParser(styles)
-    .then(_styles => loaderCallback(null, fixTemplate(_styles, source)))
+    .then(_styles => {
+      loaderCallback(null, fixTemplate(_styles, source))
+    })
     .catch((err) => {
-      console.log(`[polymerPostcssLoader][${htmlFilePath}] Warning: ${err}`);
+      this.emitWarning(err);
       loaderCallback(null, source);
     });
 };
